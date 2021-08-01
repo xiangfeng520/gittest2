fdb47507cc58f37f027b91efcccfd7270a3cea1b
xiangfeng <798238565@qq.com>
2021-08-01 16:46:52 +0800

    初次提交到本地库

---
+++ src/main/java/com/agree/git.java
@@ -0,0 +1,12 @@
+package com.agree;
+
+/**
+ * @author: xiangfeng
+ * @date: 2021年08月01日
+ */
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
